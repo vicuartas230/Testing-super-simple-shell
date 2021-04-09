0a6b6e5ccd9dfd67d9b2d900a0eb1095fef7d8ed
Brian Zapata <dzapata397@gmail.com>
2021-04-09 15:20:19 +0000

    stat Commit

--- stat.c
+++ stat.c
@@ -25,10 +25,10 @@ main(int argc, char *argv[])
     case S_IFBLK:  printf("block device\n");            break;
     case S_IFCHR:  printf("character device\n");        break;
     case S_IFDIR:  printf("directory\n");               break;
-    case S_IFIFO:  printf("FIFO/pipe\n");               break;
+    /* case S_IFIFO:  printf("FIFO/pipe\n");               break;
     case S_IFLNK:  printf("symlink\n");                 break;
     case S_IFREG:  printf("regular file\n");            break;
-    case S_IFSOCK: printf("socket\n");                  break;
+    case S_IFSOCK: printf("socket\n");                  break; */
     default:       printf("unknown?\n");                break;
     }
 
